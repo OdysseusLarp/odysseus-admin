8abcc02f3f42b88983f9861ca6c3b708de5a7465
Sampo Juustila <sampo.juustila@iki.fi>
2018-11-18 20:38:59 +0200

    Fetch taskboxes from backend server

--- src/components/TaskboxList.vue
+++ src/components/TaskboxList.vue
@@ -1,24 +1,32 @@
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="taskboxes"
-    class="elevation-1"
-    v-bind:rows-per-page-items='[ 50, 100, { "text": "$vuetify.dataIterator.rowsPerPageAll", "value": -1 } ]'
-  >
-    <template slot="items" slot-scope="props">
-      <td>{{ props.item.id }}</td>
-      <td>{{ props.item.updated_at }}</td>
-      <td>{{ props.item.version }}</td>
-      <td>{{ props.item.value }}</td>
-    </template>
-  </v-data-table>
+  <div>
+    <v-alert type="error" :value="error">Error loading data: {{ error }}</v-alert>
+    <v-data-table
+      :headers="headers"
+      :items="taskboxes"
+      class="elevation-1"
+      v-bind:loading='loading'
+      v-bind:rows-per-page-items='[ 50, 100, { "text": "$vuetify.dataIterator.rowsPerPageAll", "value": -1 } ]'
+    >
+      <template slot="items" slot-scope="props">
+        <td>{{ props.item.id }}</td>
+        <td>{{ props.item.updated_at }}</td>
+        <td>{{ props.item.version }}</td>
+        <td>{{ props.item.value }}</td>
+      </template>
+    </v-data-table>
+  </div>
 </template>
 
 
 <script>
+  import axiox from 'axios'
+
   export default {
     data () {
       return {
+        loading: true,
+        error: false,
         headers: [
           {
             text: 'Taskbox ID',
@@ -38,22 +46,35 @@
           { text: 'Value', value: 'value' },
         ],
         taskboxes: [
-          {
-            "id":"mybox",
-            "value":{"value":3.14},
-            "version":"1",
-            "created_at":"2018-11-16T18:13:04.252Z",
-            "updated_at":"2018-11-16T18:13:04.252Z"
-          },
-          {
-            "id":"abox",
-            "value":{"value":2.14},
-            "version":"10",
-            "created_at":"2018-11-16T18:13:04.252Z",
-            "updated_at":"2018-11-16T18:13:04.252Z"
-          },
         ]
       }
+    },
+
+    created () {
+      // fetch the data when the view is created and the data is already being observed
+      this.fetchData()
+    },
+    watch: {
+      // call again the method if the route changes
+      '$route': 'fetchData'
+    },
+    methods: {
+      fetchData () {
+        this.loading = true
+        this.error = null
+        axios.get("/engineering/box")
+          .then(response => {
+            this.taskboxes = response.data;
+            this.loading = false;
+            this.error = null
+          })
+          .catch(error => {
+            this.loading = false;
+            this.error = "" + error;
+          });
+      }
     }
+
+
   }
 </script>
