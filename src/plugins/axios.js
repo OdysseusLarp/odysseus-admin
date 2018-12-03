8c2705a951819555db797d1498fdc305566fe543
Sampo Juustila <sampo.juustila@iki.fi>
2018-12-03 20:38:27 +0200

    Update axios configuration

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -2,6 +2,7 @@
 
 import Vue from 'vue';
 import axios from "axios";
+import store from '../store';
 
 // Full config:  https://github.com/axios/axios#request-config
 // axios.defaults.baseURL = process.env.baseURL || process.env.apiUrl || '';
@@ -9,7 +10,7 @@ import axios from "axios";
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 let config = {
-  baseURL: "http://localhost:8888"
+  // baseURL: store.state.backend.url
   // timeout: 60 * 1000, // Timeout
   // withCredentials: true, // Check cross-site Access-Control
 };
@@ -57,5 +58,22 @@ Plugin.install = function(Vue, options) {
 };
 
 Vue.use(Plugin)
+
+function updateBackend(backend) {
+  console.log("Setting backend URI to '" + backend.uri + "' " + (backend.username ? "with":"without") + " credentials");
+  axios.defaults.baseURL = backend.uri;
+  if (backend.username) {
+    axios.withCredentials = true;
+    axios.defaults.auth = {
+      username: backend.username,
+      password: backend.password
+    };
+  } else {
+    axios.defaults.auth = undefined;
+  }
+}
+
+store.watch(state => state.backend, updateBackend);
+updateBackend(store.state.backend);
 
 export default Plugin;
