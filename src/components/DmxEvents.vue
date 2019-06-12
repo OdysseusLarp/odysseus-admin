2831f96fe3d5921e77618f5f5baf730dd6a36da4
Sampo Juustila <sampo.juustila@iki.fi>
2019-06-12 22:46:38 +0300

    feat: add DMX ship-wide announcements

--- src/components/DmxEvents.vue
+++ src/components/DmxEvents.vue
@@ -2,17 +2,32 @@
   <b-container fluid class="my-4">
     <p><strong>Fire DMX event:</strong></p>
     <p>
-    <select v-model="selectedChannel">
-      <option disabled>Choose channel</option>
-      <option v-for="c in Object.keys(channels)" :key="c" :value="c">{{c}} ({{channels[c]}})</option>
-    </select>
-    &nbsp;&nbsp;&nbsp;
-    <button @click="fireEvent">Fire event</button>
-    <button @click="reset">Reset</button>
-    (no confirmation popup)
+      <select v-model="selectedChannel">
+        <option disabled>Choose channel</option>
+        <option v-for="c in Object.keys(channels)" :key="c" :value="c">{{c}} ({{channels[c]}})</option>
+      </select>
+      &nbsp;&nbsp;&nbsp;
+      <button @click="fireEvent">Fire event</button>
+      <button @click="reset">Reset</button>
+      (no confirmation popup)
     </p>
+    <p><em>In most cases <strong>you should not</strong> fire DMX events directly! Instead perform the action which fires the DMX event.</em></p>
     <p><a href="https://docs.google.com/spreadsheets/d/1zF7ON0w8ZFF89w-btlcbMoXc25x8yOZSiLaiF7W-T5I/edit#gid=401779151" target="_blank">DMX channel specifications</a></p>
     <p><a href="https://docs.google.com/presentation/d/1nbXQE9N10Zm7uS45eW4R1VvYU4zZQ0PZbRovUq7bA5o/edit#slide=id.g4d32841109_0_0" target="_blank">Jump drive DMX event specifications</a></p>
+
+    <p style="margin-top: 3em"><strong>Fire shipwide announcement:</strong></p>
+    <p>
+      <select v-model="selectedAnnouncement">
+        <option disabled>Choose announcement</option>
+        <option v-for="c in Object.keys(announcements)" :key="c" :value="c">{{c}} ({{announcements[c]}})</option>
+      </select>
+      &nbsp;&nbsp;&nbsp;
+      <button @click="fireAnnouncement">Fire announcement</button>
+      <button @click="reset">Reset</button>
+      (no confirmation popup)
+    </p>
+    <p>DMX event on channel {{announcementChannel}} with value of the selected announcement.</p>
+
   </b-container>
 </template>
 
@@ -21,8 +36,11 @@
 
 
 <script>
-import axios from "axios";
-import io from "socket.io-client";
+import axios from "axios"
+import io from "socket.io-client"
+import announcements from "./dmx-announcements"
+
+const ANNOUNCEMENT_CHANNEL = 119
 
 export default {
   components: {
@@ -31,6 +49,9 @@ export default {
     return {
       channels: {},
       selectedChannel: undefined,
+      announcements,
+      selectedAnnouncement: undefined,
+      announcementChannel: ANNOUNCEMENT_CHANNEL,
     };
   },
   computed: {
@@ -79,8 +100,36 @@ export default {
       });
       this.reset()
     },
+
+    fireAnnouncement() {
+      if (!this.selectedAnnouncement) {
+        return
+      }
+      const name = this.selectedAnnouncement
+      const value = this.announcements[name]
+      axios.post(`/dmx/event/${ANNOUNCEMENT_CHANNEL}?value=${value}`)
+      .then(response => {
+        this.$notify({
+          title: `Fired DMX event ${ANNOUNCEMENT_CHANNEL} value ${value}`,
+          text: name,
+          type: "success",
+          duration: 10000,
+        });
+      })
+      .catch(error => {
+        console.log("Error firing event:", error);
+        this.$notify({
+          title: "Error firing DMX event",
+          text: "" + error,
+          type: "error",
+        });
+      });
+      this.reset()
+    },
+
     reset() {
       this.selectedChannel = undefined
+      this.selectedAnnouncement = undefined
     }
   }
 };
---
+++ src/components/dmx-announcements.js
@@ -0,0 +1,9 @@
+export default
+{
+  "Lorem ipsum": 1,
+  "I'm sorry, Dave. I'm afraid I can't do that.": 2,
+  "No. Try not. Do. Or do not. There is no try.": 3,
+  "It's only a flesh wound.": 4,
+  "You shall not pass.": 5,
+  "I'll be back.": 6,
+}
