c507d1acfa1ef1eb57da601cd690a053a255b594
Ilmari Karonen <nospam@vyznev.net>
2019-06-26 17:13:43 +0300

    add airlock control view

---
+++ src/views/Airlocks.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about">
+    <b-container fluid class="my-4">
+      <h1>Airlocks</h1>
+    </b-container>
+    <b-container fluid class="my-4">
+      <b-row>
+        <b-col class="border"><AirlockControl boxId="airlock_main"/></b-col>
+        <b-col class="border"><AirlockControl boxId="airlock_hangarbay"/></b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+import AirlockControl from "@/components/AirlockControl.vue";
+
+export default {
+  components: { AirlockControl },
+  computed: mapState(["backendUri"])
+};
+</script>
