0d293d85be1a9af85dd1fd2043e3bf18a139e518
Ilmari Karonen <nospam@vyznev.net>
2019-06-27 03:34:32 +0300

    more airlock tweaks

--- src/components/AirlockControl.vue
+++ src/components/AirlockControl.vue
@@ -6,20 +6,33 @@
 			<li>Last command: {{box.last_command || 'none'}} ({{timeString(box.last_command_at)}})</li>
 			<li>Pressure: <pressureRamp :rawPressure="box.pressure" :curve="box.config.pressure_curve"/></li>
 			<li>Countdown: <countdown :target="box.countdown_to"/></li>
-			<li>Linked task: {{linkedBox.id}} (status: {{linkedBox.status}})</li>
+			<li>Linked task: {{linkedBox.id}} (type: {{linkedBox.type}}, status: {{linkedBox.status}})</li>
 		</ul>
 
+		Actions:
 		<div class="button-group">
 			<b-button variant="success" @click="sendCommand('open')">Open</b-button>
 			<b-button variant="success" @click="sendCommand('close')">Close</b-button>
 			<b-button variant="success" @click="sendCommand('pressurize')">Pressurize</b-button>
 			<b-button variant="success" @click="sendCommand('depressurize')">Depressurize</b-button>
+			<b-button variant="warning" @click="sendCommand('stop')">Stop</b-button>
+			<b-button variant="warning" @click="sendCommand('malfunction')">Malfunction</b-button>
+		</div>
+
+		Force status (does not fire DMX events!):
+		<div class="button-group">
+			<b-button variant="danger" @click="forceStatus('open')">Open</b-button>
+			<b-button variant="danger" @click="forceStatus('closed')">Closed</b-button>
+			<b-button variant="danger" @click="forceStatus('vacuum')">Vacuum</b-button>
 		</div>
 
 	</b-container>
 </template>
 
 <style lang="scss">
+.button-group {
+	margin-bottom: 1em;
+}
 </style>
 
 <script>
@@ -34,23 +47,30 @@ export default {
 	},
 	computed: {
 		box () {
-			return this.getBox(this.boxId) || DEFAULT_BOX
+			return this.getBlob('box', this.boxId) || DEFAULT_BOX
 		},
 		linkedBox () {
-			const boxId = this.box.config.linked_task_id
-			if (!boxId) return { id: 'none', status: 'n/a' }
-			return this.getBox() || { id: boxId, status: 'NOT FOUND' }
+			const config = this.box.config || {}
+			const type = config.linked_task_type || 'box', id = config.linked_task_id
+			if (!id) return { type: type, id: 'none', status: 'n/a' }
+			return this.getBlob(type, id) || { type: type, id: id, status: 'NOT FOUND' }
 		},
   },
 	methods: {
-		getBox (boxId) {
-			return this.$store.state.dataBlobs.find(e => e.type === 'box' && e.id === boxId)
+		getBlob (type, id) {
+			return this.$store.state.dataBlobs.find(e => e.type === type && e.id === id)
 		},
 		sendCommand (command) {
 			console.log(`Send command ${command} to ${this.boxId}...`)
 			axios.patch(`/data/box/${this.boxId}?force=true`, {command: command})
 				.then(() => console.log(`Command ${command} sent to ${this.boxId}`))
 				.catch((err) => console.log(`Command ${command} to ${this.boxId} failed:`, err))
+		},
+		forceStatus (status) {
+			console.log(`Forcing ${this.boxId} to state ${status}...`)
+			axios.patch(`/data/box/${this.boxId}?force=true`, {status: status, command: 'stop'})
+				.then(() => console.log(`Forced ${this.boxId} to state ${status}`))
+				.catch((err) => console.log(`Forcing ${this.boxId} to state ${status} failed:`, err))
 		},
 		timeString (timestamp) {
 			if (!timestamp) return 'never'
