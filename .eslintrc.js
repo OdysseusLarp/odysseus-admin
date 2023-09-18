0fef6f5780854abdcfdfdd89754e7cef6e69d903
Nico Hagelberg <nicou@nicou.me>
2023-09-18 10:14:22 +0300

    Update packages to work with Node 18

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,23 +3,32 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
     "vue/no-unused-vars": "off",
-    "vue/no-side-effects-in-computed-properties": "off"
+    "vue/no-side-effects-in-computed-properties": "off",
+    "vue/multi-word-component-names": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn", // or "error"
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+      },
+    ],
   },
 
   root: true,
 
   env: {
-    node: true
+    node: true,
   },
 
   extends: [
     "plugin:vue/recommended",
     "plugin:vue/essential",
     "@vue/prettier",
-    "@vue/typescript"
+    "@vue/typescript",
   ],
 
   parserOptions: {
-    parser: "@typescript-eslint/parser"
-  }
+    parser: "@typescript-eslint/parser",
+  },
 };
