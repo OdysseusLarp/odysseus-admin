7a5a232c718d4823da4519f457d2bddb0b4dd24b
Nico Hagelberg <nicou@nicou.me>
2024-07-03 21:01:01 +0300

    Hide automated operations by default

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,10 +1,6 @@
 import { format } from "date-fns";
 
-export function pushError(
-  errors: string[] = [],
-  error: Error | string,
-  $notify?: Function,
-) {
+export function pushError(errors: string[] = [], error: Error | string, $notify?: Function) {
   errors.push(`[${format(Date.now(), "HH:mm:ss")}] ${error}`);
   if ($notify)
     $notify({
@@ -12,4 +8,13 @@ export function pushError(
       text: "" + error,
       type: "error",
     });
+}
+
+const AUTOMATED_SAMPLES = ["MATERIAL_SAMPLE", "MICROSCOPE_SAMPLE", "AGE", "HISTORY_SAMPLE", "XRF_SAMPLE"];
+
+export function isAutomatedSample(sample: any) {
+  if (!sample || !sample.additional_type) {
+    return false;
+  }
+  return AUTOMATED_SAMPLES.includes(sample.additional_type);
 }
